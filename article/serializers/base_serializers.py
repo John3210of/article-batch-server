7eae093ea3907e70d9518ed553d93e5b22b6b11a
John3210of <john3210of@gmail.com>
2025-01-10 16:34:55 +0900

    feat : Convert all API responses to camelCase globally

--- article/serializers/base_serializers.py
+++ article/serializers/base_serializers.py
@@ -1,10 +1,10 @@
+from inflection import camelize
 from rest_framework import serializers
 
 class BaseResponseSerializer(serializers.Serializer):
     """
     공통 응답 구조를 위한 Serializer.
     모든 API 응답은 이 구조를 따릅니다.
-    기본적으로 성공(success=True)으로 설정됩니다.
     """
     success = serializers.BooleanField(default=True)
     errorCode = serializers.CharField(allow_null=True, required=False, default=None)
@@ -12,9 +12,12 @@ class BaseResponseSerializer(serializers.Serializer):
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        if 'success' not in representation:
-            representation['success'] = True
-        if 'errorCode' not in representation:
-            representation['errorCode'] = None
 
-        return representation
+        def convert_keys_to_camel_case(data):
+            if isinstance(data, dict):
+                return {camelize(key, uppercase_first_letter=False): convert_keys_to_camel_case(value) for key, value in data.items()}
+            elif isinstance(data, list):
+                return [convert_keys_to_camel_case(item) for item in data]
+            return data
+
+        return convert_keys_to_camel_case(representation)
