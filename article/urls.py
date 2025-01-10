e34ce1d9a65a77738f18961fdb8074522ef390dd
John3210of <john3210of@gmail.com>
2025-01-10 16:09:51 +0900

    fix : user-category bug fix

--- article/urls.py
+++ article/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from article.views.user_info import UserCategoryViewSet
+from article.views.user_info import UserCategoryViewSet,UserScheduleViewSet
 from article.views import HealthCheckView
 from article.views.article import CategoryViewSet
+
 router = DefaultRouter()
-router.register(r'user-category', UserCategoryViewSet, basename='user-category')
 router.register(r'category', CategoryViewSet, basename='category')
+router.register(r'user-category', UserCategoryViewSet, basename='user-category')
+router.register(r'user-schedule', UserScheduleViewSet, basename='user-schedule')
+
 urlpatterns = [
     path('health/', HealthCheckView.as_view(), name='health-check'),
+    path('user-category/<str:user_email>/', UserCategoryViewSet.as_view({'get': 'retrieve'})),
     path('', include(router.urls)),
 ]
--- article/views/user_info.py
+++ article/views/user_info.py
@@ -1,149 +1,106 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from article.models.user_info.user_category import UserCategory
-from article.serializers.user_info.user_info_serializers import UserCategorySerializer, UserCategoryCreateSerializer
-from article.serializers.base_serializers import BaseResponseSerializer
 from drf_yasg.utils import swagger_auto_schema
+from article.models.user_info.user_schedule import UserSchedule
+from article.models.user_info.user_category import UserCategory
+from article.serializers.base_serializers import BaseResponseSerializer
+from article.serializers.user_info.user_info_serializers import (
+    UserCategoryCreateSerializer,UserScheduleCreateSerializer,UserCategorySerializer,UserScheduleSerializer
+)
+
+def handle_error_response(errors, error_code, status_code):
+    '''
+    #TODO seperate!
+    '''
+    response_data = {
+        "success": False,
+        "errorCode": error_code,
+        "data": errors
+    }
+    return Response(BaseResponseSerializer(response_data).data, status=status_code)
 
 class UserCategoryViewSet(viewsets.ViewSet):
-    #TODO Error handle
     """
-    UserCategory 관련 Viewset입니다.
-    기본 메서드를 사용하며 user_email을 lookup_field로 설정합니다.
+    UserCategory 관련 ViewSet입니다.
     """
     queryset = UserCategory.objects.all()
-    serializer_class = UserCategorySerializer
     lookup_field = 'user_email'
-
-    @swagger_auto_schema(tags=["User Category API"])
+    
+    @swagger_auto_schema(
+        tags=["User Category API"],
+        responses={
+            200: BaseResponseSerializer,
+            400: BaseResponseSerializer,
+        },
+    )
     def list(self, request, *args, **kwargs):
         """
         모든 UserCategory를 조회합니다.
         """
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
+        queryset = self.queryset
+        serializer = UserCategorySerializer(queryset, many=True)
         response_data = {
             "data": serializer.data
         }
         return Response(BaseResponseSerializer(response_data).data, status=status.HTTP_200_OK)
 
-    @swagger_auto_schema(tags=["User Category API"])
-    def retrieve(self, request, *args, **kwargs):
+    @swagger_auto_schema(
+    tags=["User Category API"],
+    responses={
+        200: BaseResponseSerializer,
+        400: BaseResponseSerializer,
+        404: BaseResponseSerializer,
+    },
+    )
+    def retrieve(self, request, user_email=None):
         """
         특정 UserCategory를 조회합니다.
         """
         try:
-            instance = self.get_object()
-            serializer = self.get_serializer(instance)
+            categories = UserCategory.objects.filter(user_email=user_email)
+            if not categories.exists():
+                return handle_error_response({"message": "UserCategory not found"}, "ERR404", status.HTTP_404_NOT_FOUND)
+
+            serializer = UserCategorySerializer(categories, many=True)
             response_data = {
                 "data": serializer.data
             }
             return Response(BaseResponseSerializer(response_data).data, status=status.HTTP_200_OK)
-        except UserCategory.DoesNotExist:
-            response_data = {
-                "success": False,
-                "errorCode": "ERR404",
-                "data": {"message": "UserCategory not found"}
-            }
-            return Response(BaseResponseSerializer(response_data).data, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as e:
+            return handle_error_response({"message": str(e)}, "ERR500", status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
     @swagger_auto_schema(
-        request_body=UserCategoryCreateSerializer,
-        responses={
-            201: BaseResponseSerializer,
-            400: BaseResponseSerializer
-        },
-        tags=["User Category API"]
-    )
+    request_body=UserCategoryCreateSerializer,
+    responses={
+        201: BaseResponseSerializer,
+        400: BaseResponseSerializer,
+    },
+    tags=["User Category API"]
+)
     def create(self, request, *args, **kwargs):
         """
-        새로운 UserCategory를 생성합니다.
+        새로운 UserCategory를 생성하거나 업데이트합니다.
         """
         serializer = UserCategoryCreateSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            response_data = {
-                "data": serializer.data
-            }
-            return Response(BaseResponseSerializer(response_data).data, status=status.HTTP_201_CREATED)
+        if not serializer.is_valid():
+            return handle_error_response(serializer.errors, "ERR400", status.HTTP_400_BAD_REQUEST)
 
+        user_email = serializer.validated_data['user_email']
+        new_category_ids = serializer.validated_data['categoryIds']
+        existing_user_categories = UserCategory.objects.filter(user_email=user_email)
+        existing_category_ids = set(existing_user_categories.values_list('category_id', flat=True))
+        categories_to_add = set(new_category_ids) - existing_category_ids
+
+        categories_to_deactivate = existing_category_ids - set(new_category_ids)
+        UserCategory.objects.filter(user_email=user_email, category_id__in=categories_to_deactivate).update(is_activated=False)
+        for category_id in categories_to_add:
+            UserCategory.objects.create(user_email=user_email, category_id=category_id, is_activated=True)
+
+        updated_user_categories = UserCategory.objects.filter(user_email=user_email,is_activated=True)
+        response_serializer = UserCategorySerializer(updated_user_categories, many=True)
         response_data = {
-            "success": False,
-            "errorCode": "ERR400",
-            "data": serializer.errors
+            "data": response_serializer.data
         }
-        return Response(BaseResponseSerializer(response_data).data, status=status.HTTP_400_BAD_REQUEST)
-
-    @swagger_auto_schema(
-        request_body=UserCategoryCreateSerializer,
-        responses={
-            200: BaseResponseSerializer,
-            400: BaseResponseSerializer,
-            404: BaseResponseSerializer
-        },
-        tags=["User Category API"]
-    )
-    def partial_update(self, request, *args, **kwargs):
-        """
-        userEmail과 categoryId 리스트를 받아 is_activated 상태를 토글합니다.
-        """
-        instance = self.get_object()
-        user_email = instance.user_email
-        category_ids = request.data.get('categoryIds')
-
-        if not category_ids or not isinstance(category_ids, list):
-            response_data = {
-                "success": False,
-                "errorCode": "ERR400",
-                "data": {"message": "categoryIds must be a list"}
-            }
-            return Response(BaseResponseSerializer(response_data).data, status=status.HTTP_400_BAD_REQUEST)
-
-        updated_categories = []
-        not_found_categories = []
-
-        for category_id in category_ids:
-            try:
-                user_category = UserCategory.objects.get(user_email=user_email, category=category_id)
-                user_category.is_activated = not user_category.is_activated
-                user_category.save()
-                updated_categories.append({
-                    "categoryId": category_id,
-                    "isActivated": user_category.is_activated
-                })
-            except UserCategory.DoesNotExist:
-                not_found_categories.append(category_id)
-
-        response_data = {
-            "success": True,
-            "errorCode": None,
-            "data": {
-                "updatedCategories": updated_categories,
-                "notFoundCategories": not_found_categories
-            }
-        }
-        return Response(BaseResponseSerializer(response_data).data, status=status.HTTP_200_OK)
-    
-    @swagger_auto_schema(auto_schema=None) 
-    def update(self, request, *args, **kwargs):
-        """
-        PUT 메서드는 비활성화 합니다.
-        """
-        response_data = {
-            "success": False,
-            "errorCode": "ERR405",
-            "data": {"message": "Method Not Allowed"}
-        }
-        return Response(BaseResponseSerializer(response_data).data, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-    @swagger_auto_schema(auto_schema=None) 
-    def destroy(self, request, *args, **kwargs):
-        """
-        DELETE 메서드는 비활성화 합니다.
-        """
-        response_data = {
-            "success": False,
-            "errorCode": "ERR405",
-            "data": {"message": "Method Not Allowed"}
-        }
-        return Response(BaseResponseSerializer(response_data).data, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return Response(BaseResponseSerializer(response_data).data, status=status.HTTP_201_CREATED)
