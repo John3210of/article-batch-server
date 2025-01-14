a79f7cd600cdaff8e4e58bdf04b1c077d0923ca6
John3210of <john3210of@gmail.com>
2025-01-14 10:48:21 +0900

    feat : feature article crud

--- article/urls.py
+++ article/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from article.views.user_info import UserCategoryViewSet,UserScheduleViewSet
 from article.views import HealthCheckView
-from article.views.article import CategoryViewSet
+from article.views.article import CategoryViewSet,ArticleViewSet
 
 router = DefaultRouter()
 router.register(r'category', CategoryViewSet, basename='category')
+router.register(r'article', ArticleViewSet, basename='article')
 router.register(r'user-category', UserCategoryViewSet, basename='user-category')
 router.register(r'user-schedule', UserScheduleViewSet, basename='user-schedule')
 
