aebdd1dcdce8423e7967f44950dd41dbe4cba4b7
John3210of <john3210of@gmail.com>
2025-02-06 16:07:17 +0900

    test

--- django_server/urls.py
+++ django_server/urls.py
@@ -22,7 +22,7 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Canary V4 Django Project API",
+        title="Canary V5 Django Project API",
         default_version='v1',
         description="API documentation for the Django project",
         terms_of_service="localhost",
